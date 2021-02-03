bb00ada362bcadb338b5f7638ff0ca5a27145587
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2021-02-03 10:07:01 -0800

    Fix practice description.

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -1,10 +1,9 @@
 /**
- * Practice: Building functions
+ * Practice: Building objects
  *
- * - Create a basic function declaration - you’ll remember that’s where we say “function” followed by the name of the function.
- * - In the body of the function declaration, do something to an element in the DOM. For this you can use any of the techniques we’ve covered earlier - finding an element using querySelector, adding a class, whatever you like.
- * - Call the function declaration so the action takes place.
- * - Create a basic function expression - that’s when you define a variable and place an anonymous function inside
- * - Do the same as above - find an element, make a change to it, call the function, make sure it works.
- * - Finally, create an arrow function, make it do something, and call it.
+ * - Create JavaScript objects based on objects in your current environment.
+ * - Give each object an identifiable name.
+ * - Create properties to describe the objects and set their values.
+ * - Find an object that has another object inside of it to create a nested object.
+ * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
