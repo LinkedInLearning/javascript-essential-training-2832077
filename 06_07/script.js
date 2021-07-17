ae9d5692e3b88d33e29684936edc76a54cc2e3e5
tuannguyenvn <tuannguyen.de@gmail.com>
2021-07-17 17:25:11 +0200

    compare

--- 06_07/script.js
+++ 06_07/script.js
@@ -4,12 +4,12 @@
  */
 
 let a = 5;
-let b = 4;
+let b = '5';
 
 console.log(`let a: ${a} (${typeof a})`);
 console.log(`let b: ${b} (${typeof b})`);
 
-if (a == b) {
+if (a === b) {
   console.log(`Match! let a and let b are the same value.`);
 } else {
   console.error(`No match: let a and let b are NOT same value.`);
