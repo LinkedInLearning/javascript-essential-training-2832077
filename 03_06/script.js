f6d2c9a51bf4a2cb41762afbe0f3c8348ee19aee
ahmedseragcodes <ahmedserag1@gmail.com>
2021-10-02 19:37:28 -0400

    finished movie 3 chapter 7 practice exercise

--- 03_06/script.js
+++ 03_06/script.js
@@ -21,15 +21,31 @@ const backpack = {
   },
 };
 
+//REFERENCE VALUES
 
-console.log("The backpack object initially:", backpack);
+// console.log("The backpack object initially:", backpack);
 
-const secondBackpack = backpack;
+// const secondBackpack = backpack;
 
-console.log("the secondBackpack object initially", secondBackpack);
+// console.log("the secondBackpack object initially", secondBackpack);
 
-secondBackpack.newStrapLength(30,30);
+// secondBackpack.newStrapLength(30,30);
 
-console.log("the backpack object following referance change", backpack);
+// console.log("the backpack object following referance change", backpack);
 
-console.log("the secondBackpack object following function call", secondBackpack);
+// console.log("the secondBackpack object following function call", secondBackpack);
+
+//BRACKET NOTATION 
+
+console.log("Backpack Name", backpack["name"]);
+
+const nickName = "name"
+
+// backpack["name"] = nickName;
+
+// console.log("Backpack Name", backpack["name"]);
+
+console.log(backpack[nickName])
+
+/* So by attaching the property name as a string to the const nickName, 
+we can now use bracket notation to access that properties value */
