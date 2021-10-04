b7c0c36e81e520eb85e8d0578067b80daa48e306
ahmedseragcodes <ahmedserag1@gmail.com>
2021-10-04 11:22:31 -0400

    practiced DOM surgery with querySelector and querySelectorAll

--- 05_02/script.js
+++ 05_02/script.js
@@ -49,3 +49,16 @@ const content = `
 `;
 
 main.innerHTML = content;
+
+
+const featuresList = document.querySelector(".backpack__features");
+
+console.log("Here's a list of features", featuresList);
+
+const allFeatures = document.querySelectorAll(".backpack__features li");
+
+console.log("All Features", allFeatures);
+
+allFeatures.forEach((feat)=>{
+  console.log(feat);
+})
