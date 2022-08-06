f9df11e7e0ce6f1fb1fa0f411eac11f61fd8eaca
Saurav Mishra <saurav.mishra@live.com>
2022-08-06 19:36:03 +0530

    Update script.js

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,45 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+ const updateCar = (update) => {
+    let main = document.querySelector("main"); // main is an element
+    main.innerHTML = markup(car);
+    console.info(update);
+  };
+
+ const car = {
+    Company: "Volkswagen",
+    Model: "Polo",
+    Color: "Red",
+    Transmission: "Manual",
+    Dimension: {
+      length: 500,
+      width: 200,
+    },
+    DriveMode: "2WD",
+    Fuel: "Petrol",
+    updateColor: function (newColor) {
+      this.Color = newColor;
+      updateCar(newColor);
+    },
+  };
+
+  const markup = (car) => {
+    return `
+    <div>
+      <h3>${car.Company} ${car.Model}</h3>
+      <ul>
+        <li>Color: ${car.Color}</li>
+        <li>Transmission: ${car.Transmission}</li>
+        <li>Drive Mode: ${car.DriveMode}</li>
+        <li>Dimension: Length: ${car.Dimension.length}, Width: ${ car.Dimension.width} </li>
+        <li>Fuel: ${car.Fuel}</li>
+      </ul>
+    </div>
+  `;
+  };
+  
+  const main = document.createElement("main");
+  main.innerHTML = markup(car);
+  document.body.appendChild(main);
