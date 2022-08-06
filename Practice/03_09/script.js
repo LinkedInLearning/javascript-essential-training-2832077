1a03ebbc0bc42fb6b35af1ae500cd8997c009d3c
Saurav Mishra <saurav.mishra@live.com>
2022-08-06 20:11:52 +0530

    adding new functions

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -7,6 +7,12 @@
  * - Test the method by sending new values and checking the properties in the console.
  */
 
+const updateBackpack = (update) => {
+  let main = document.querySelector("main"); // main is an element
+  main.innerHTML = markup(backpack);
+  console.info(update);
+};
+
 const backpack = {
   name: "Everyday Backpack",
   volume: 30,
@@ -22,5 +28,35 @@ const backpack = {
   newStrapLength: function (lengthLeft, lengthRight) {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
+    updateBackpack(`Strap lengths updated.`);
+  },
+  updateColor: function (newColor) {
+    this.color = newColor;
+    updateBackpack(newColor);
+  },
+  updateVolume: function (newVolume) {
+    this.volume = newVolume;
+    updateBackpack(newVolume);
   },
 };
+
+const markup = (backpack) => {
+  return `
+  <div>
+	<h3>${backpack.name}</h3>
+	<ul>
+	  <li>Volume: ${backpack.volume}</li>
+	  <li>Color: ${backpack.color}</li>
+	  <li>Number of pockets: ${backpack.pocketNum}</li>
+	  <li>Strap lengths: L: ${backpack.strapLength.left}, R: ${
+    backpack.strapLength.right
+  } </li>
+	  <li>Top lid: ${backpack.lidOpen ? "Open" : "Closed"}</li>
+	</ul>
+  </div>
+`;
+};
+
+const main = document.createElement("main");
+main.innerHTML = markup(backpack);
+document.body.appendChild(main);
