8174442a0036a9bb115c77a6d1ac26a0819735fe
Ilma Muttaqien <ilmamutaien13@gmail.com>
2022-11-22 20:13:58 +0700

    Create riceCooker object

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,18 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const riceCooker = {
+  name: "Advance Rice Cooker",
+  color: "white",
+  price: 190000,
+  size: {
+    width: 30,
+    height: 30,
+    length: 30,
+  },
+  capacity: 1.8,
+  isLidOpen: false,
+  isCooking: false,
+  isConnected: false,
+};
